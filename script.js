d3432c5f02e72cfd74d3cf4abf3d7d44611d33cb
Meenah Dai'Jean <meenahdaijean@gmail.com>
2021-07-13 20:55:14 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// add variables here
+let price; 
+let drink; 
+
+function checkout(choice) {
+  document.querySelector("#checkout").classList.remove("hidden");
+  document.querySelector("#checkout-img").src = `${choice}.jpg`; //look for other concatenation
+
+  // add the conditional below
+  if (choice ==="latte"){
+    drink="latte";
+    price= 4.79;
+  } else if(choice ==="cappuccino"){
+    price= 4.79;
+    drink="cappuccino";
+} else if(choice ==="iced-coffee"){ 
+  price= 3.79; 
+  drink="iced-coffee"; 
+ }
+  
+
+  document.querySelector("#selected-drink").innerText = drink;
+  document.querySelector("#price").innerText = "$" + price.toFixed(2);
+}
+
+function receipt() {
+  document.querySelector("#checkout").classList.add("hidden");
+  document.querySelector("#receipt").classList.remove("hidden");
+
+  let quantity = parseInt(document.querySelector("#quantity1").value);
+
+  // add quantity and total calculation below!
+  let total= quantity * price;  
+
+  document.querySelector("#drink").innerText = drink;
+  document.querySelector("#total").innerText = "$" + total.toFixed(2);
+  document.querySelector("#quantity2").innerText = quantity;
+  document.querySelector("#price2").innerText = "$" + price;
+}
+
+function restart() {
+  document.querySelector("#receipt").classList.add("hidden");
+  document.querySelector("#quantity1").value = 0;
+}
